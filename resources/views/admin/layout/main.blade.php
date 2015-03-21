f66363f3c6701902c4f16b4f6850659d250cb2bb
vesko-vujovic <veskovujovic@yahoo.com>
2015-03-21 12:12:54 +0100

    First commit

---
+++ resources/views/admin/layout/main.blade.php
@@ -0,0 +1,10 @@
+{!! Html::style('css/bootstrap.css') !!}
+{!! Html::style('css/app.css') !!}
+{!! Html::style('css/bootstrap-theme.css') !!}
+{!! Html::style('css/adminLog.css') !!}
+{!! Html::script('js/jquery.min.js') !!}
+{!! Html::script('js/bootstrap.js') !!}
+
+
+@yield('log')
+
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,13 @@
+{!! Html::style('css/bootstrap.css') !!}
+{!! Html::style('css/app.css') !!}
+{!! Html::style('css/bootstrap-theme.css') !!}
+{!! Html::script('js/jquery.min.js') !!}
+{!! Html::script('js/bootstrap.js') !!}
+{!! Html::script('js/ajax.js') !!}
+
+@yield('nav')
+
+@yield('article')
+
+
+
